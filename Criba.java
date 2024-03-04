bf241825e0529518dbe7557701eaeb655cfa6d26
Eriós VHR <erios.inf@gmail.com>
2024-03-04 22:44:52 +0100

    Update Criba.java

--- Criba.java
+++ Criba.java
@@ -1,8 +1,36 @@
-// EDD Tarea 4
 import java.util.Scanner;
-public class Criba
-{
-    // Generar números primos de 1 a max
+public class Criba {
+    public void inicializa(boolean array[]){
+        for ( int i=2; i<array.length; i++)
+            array[i] = true;
+// Eliminar el 0 y el 1, que no son primos
+        array[0] = array[1] = false;}
+    public void criba (boolean array[]){
+        for (int i=2; i<Math.sqrt(array.length)+1 ; i++) {
+            if (array[i]) {
+// Eliminar los múltiplos de i
+                for (int j=2*i; j<array.length; j+=i)
+                    array[j] = false;
+            }
+        }
+    }
+    public int cuentaPrimos(boolean array[]){
+        int cuentaPrimos = 0;
+        for (int i=0; i<array.length; i++) {
+            if (array[i])
+                cuentaPrimos++;
+        }
+        return cuentaPrimos;
+    }
+    public int[] totalPrimos(boolean array[]){
+        int[] primos = new int[cuentaPrimos(array)];
+        for (int i=0, j=0; i< array.length; i++) {
+            if (array[i])
+                primos[j++] = i;
+        }
+        return primos;
+    }
+  
     public static int[] generarPrimos (int max)
     {
         int i,j;
@@ -10,32 +38,9 @@ public class Criba
 // Declaraciones
             int dimArray = max + 1; // Tamaño del array
             boolean[] esPrimo = new boolean[dimArray];
-// Inicializar el array
-            for (i=2; i<dimArray; i++)
-                esPrimo[i] = true;
-// Eliminar el 0 y el 1, que no son primos
-            esPrimo[0] = esPrimo[1] = false;
-// Criba
-            for (i=2; i<Math.sqrt(dimArray)+1 ; i++) {
-                if (esPrimo[i]) {
-// Eliminar los múltiplos de i
-                    for (j=2*i; j<dimArray; j+=i)
-                        esPrimo[j] = false;
-                }
-            }
-// ¿Cuántos primos hay?
-            int cuentaPrimos = 0;
-            for (i=0; i<dimArray; i++) {
-                if (esPrimo[i])
-                    cuentaPrimos++;
-            }
-// Rellenar el vector de números primos
-            int[] primos = new int[cuentaPrimos];
-            for (i=0, j=0; i<dimArray; i++) {
-                if (esPrimo[i])
-                    primos[j++] = i;
-            }
-            return primos;
+            inicializa(esPrimo);
+            criba(esPrimo);
+            return totalPrimos(esPrimo);
         } else { // max < 2
             return new int[0];
 // Vector vacío
